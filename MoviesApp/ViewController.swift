c86da2358440a2ddacc6a0cdc7ecd02d5dc07c6d
Raghav Sai Cheedalla <raghavsai.cheedalla@dominionenterprises.com>
2018-10-21 14:00:34 -0400

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Raghav Sai Cheedalla on 10/21/18.
+//  Copyright © 2018 Swift Enthusiast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
